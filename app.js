cdbcc80842a3a18fdb6e421b3b4ddf812063f446
Mohammed Propelius <mohammed.kharodawala@propelius.tech>
2023-02-02 15:52:16 +0530

    server

--- app.js
+++ app.js
@@ -6,8 +6,5 @@ const userRoute = require('./routes/userRoutes')
 app.use('/api/v1/tours',tourRoute)
 app.use('/api/v1/users',userRoute)
 // listing on port
-const port = 3000
-app.listen(port,()=>{
-    console.log(`Listing on the ${port}`)
-})
 
+module.exports = app
